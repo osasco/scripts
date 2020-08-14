df34ea2a35c1dec8ee687d1a4f011cb8654e1e01
osasco <40818544+osasco@users.noreply.github.com>
2020-08-13 22:09:40 -0300

    Update opkg-update.sh

--- opkg-update.sh
+++ opkg-update.sh
@@ -2,6 +2,6 @@
 /bin/opkg update
 PKTS=$(/bin/opkg list-upgradable | /bin/sed -En "s/(.*) - .* - .*/\1/p")
 if [ -n "$PKTS" ]; then
-	/bin/echo "$PKTS" | /usr/bin/xargs opkg upgrade
+	/bin/echo "$PKTS" | /usr/bin/xargs /bin/opkg upgrade
 	/usr/bin/find /etc -type f -iname "*-opkg" -print0 | /usr/bin/xargs -0r rm
 fi
